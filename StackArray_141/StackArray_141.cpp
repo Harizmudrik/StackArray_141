0343864d03b115fd6541e5b7a4372fc26650f4cf
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-06-15 14:58:01 +0700

    Menambahkan string push

--- StackArray_141/StackArray_141.cpp
+++ StackArray_141/StackArray_141.cpp
@@ -9,7 +9,20 @@ class StackArray {
 private:
     string stack_array[5];
     int top;
-    E
+public:
+    //constructor
+    StackArray() {
+        top = -1;
+    }
+
+    string push(string element) {
+        if (top == 4) { //step 1
+            cout << "Number of data exceeds the limit." << endl;
+            return;
+        }
+
+
+    }
 };
 
 int main()
